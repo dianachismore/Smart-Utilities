04e8d492795747c75818efa8082936f8d9f8079f
Lavinia-CristinaRus <lavinia.rus20@gmail.com>
2022-10-28 13:51:03 +0300

    created post method for registration

--- app.js
+++ app.js
@@ -2,8 +2,18 @@ const express = require('express');
 require('dotenv').config();
 
 require('./models/db');
-
+const User = require('./models/user')
 const app = express();
+
+app.post('/register', async (req, res) => {
+    const user = await User({
+        fullname: 'John Doe',
+        email: 'john@gmail.com',
+        password: 'password',
+        apartment: '56'
+    });
+    res.json(user);
+});
 
 app.get('/', (req,res) => {
     res.send('Initial message');
