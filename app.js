4e01eb19a6d5c3399e4c8506663a16f7e581738d
Lavinia-CristinaRus <lavinia.rus20@gmail.com>
2022-10-28 12:53:01 +0300

    connection to db established

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 const express = require('express');
+require('dotenv').config();
+
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI).then(() => {
+    console.log('db connection is successful!')
+}).catch(err => console.log(err.message));
+
 const app = express();
 
 app.get('/', (req,res) => {
@@ -6,3 +13,4 @@ app.get('/', (req,res) => {
 });
 
 app.listen(8000, () => console.log("server is up and running"));
+
